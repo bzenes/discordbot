32d09179272bccd2f3a34a350129a2c7f09e0be1
Enes Boz <32651471+bzenes@users.noreply.github.com>
2022-04-13 02:07:43 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 
-client.login("")
+client.login("") // bot token 
 
 client.on("ready", () => {
 
-    let sunucu = client.guilds.cache.get("")
-    let role = sunucu.roles.cache.get("")
+    let sunucu = client.guilds.cache.get("") // sunucu id
+    let role = sunucu.roles.cache.get("") // renginin değişmesi gereken rol id
     var colors = ['f22929', 'f26229', 'f2a529', 'f2e129', 'cdf229', 'a5f229', '65f229', '29f22c', '29f269',
         '29f294', '29f2bc', '29f2f2', '29acf2', '2965f2', '2936f2', '4029f2', '5b29f2', '7d29f2', 'a229f2', 'ca29f2', 'f229e8', 'f22979', 'f22955'
-    ];
+    ]; // hex cinsinden renk listesi
     var renkSira = 0;
 
     setInterval(() => {
